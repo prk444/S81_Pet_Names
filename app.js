4577adced6efc8056085c242119326d2db3df0e6
raghava <prk419918@gmail.com>
2025-02-24 11:21:38 +0530

    kk

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express=require('express');
 const app=express();
 const connection = require('./connection');
 require("dotenv").config();
+let mongoose = require('mongoose');
+let connect=mongoose.connect(process.env.mongo_url)
 
 app.get('/',(req,res)=>{
-    res.send('Hello World');
+    const dbStatus = mongoose.connection.readyState === 1 ? 'Connected' : 'Not Connected';
+    res.send(`Hello World. Database connection status: ${dbStatus}`);
 });
 
 
